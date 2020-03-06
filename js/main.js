ec1fb51a026afdfa828d7b21da6ecae569bdf2ab
BC22 <ngquangnhutquynh@gmail.com>
2020-03-06 12:28:06 +0700

    Thay đổi scrollbar

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ $(function () {
     $(".button").click(function () {
         $(".button-active").attr('class', 'button');
         $(this).attr('class', 'button-active');
-    })
+    });
+
+    $($(".button")[0]).attr("class", "button-active");
 })
