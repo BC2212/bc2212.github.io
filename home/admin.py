f9e370e41742b29e7baaf3efef9d243e7bdbb171
BC2212 <ngquangnhutquynh@gmail.com>
2020-06-24 09:01:26 +0700

    Thêm post vào home

--- home/admin.py
+++ home/admin.py
@@ -6,6 +6,6 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'date']
     list_filter = ['date']
-    search_fields = ['title', 'date']
+    search_fields = ['title']
 
 admin.site.register(Post, PostAdmin)
--- home/models.py
+++ home/models.py
@@ -8,4 +8,4 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return Post.title
+        return self.title
