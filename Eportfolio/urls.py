f1198a13d75dc81f154504a88e77ce1b07020c91
BC2212 <ngquangnhutquynh@gmail.com>
2020-06-24 09:12:22 +0700

    Chỉnh sửa model

--- Eportfolio/urls.py
+++ Eportfolio/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('r^tinymce/', include('tinymce.urls')),
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
 ]
--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    body = models.TextField()
+    content = HTMLField()
     date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
